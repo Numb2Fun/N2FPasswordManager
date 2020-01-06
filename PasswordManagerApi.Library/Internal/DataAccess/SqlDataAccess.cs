76715b54598539caf29c6ca90bf8ab6167fbd943
ABRAHAMMER-PC\Abrahammer <m.a.pugh4289@gmail.com>
2020-01-06 13:07:33 -0500

    Add Data Access to Api Library

---
+++ PasswordManagerApi.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordManagerApi.Library.Internal.DataAccess
+{
+    internal class SqlDataAccess
+    {
+
+    }
+}
