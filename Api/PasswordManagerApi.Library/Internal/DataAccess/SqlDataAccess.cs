67230419acfdd72cf260f8138c93026335929453
ABRAHAMMER-PC\Abrahammer <m.a.pugh4289@gmail.com>
2020-01-06 12:22:17 -0500

    Add ConnectionString To Web.Config

---
+++ Api/PasswordManagerApi.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace PasswordManagerApi.Library.Internal.DataAccess
+{
+    internal class SqlDataAccess
+    {
+
+        public string GetConnectionString(string connectionName)
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+    }
+}
