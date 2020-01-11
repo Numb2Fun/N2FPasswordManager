b6bc5db029c2736d6caac910dea152114c56ded3
ABRAHAMMER-PC\Abrahammer <m.a.pugh4289@gmail.com>
2020-01-11 18:37:50 -0500

    Add Profile-Related Tables To Db Project
    Adds 'Profiles' and 'Categories' tables to existing identity db project.

---
+++ src/PasswordManager.Data/dbo/Tables/Profiles.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Profiles]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[UserId] NVARCHAR(128) NOT NULL,
+	[CategoryId] INT NULL,
+	[Title] NVARCHAR(128) NOT NULL,
+	[Website] NVARCHAR(256) NULL,
+	[LoginName] NVARCHAR(256) NULL,
+	[Password] NVARCHAR(256) NULL,
+	[SignUpEmail] NVARCHAR(256) NULL,
+	[LastUpdated] DATETIME2 NOT NULL DEFAULT getutcdate(),
+	[Notes] NVARCHAR(MAX) NULL,
+	CONSTRAINT [FK_Profile_ToAspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]),
+	CONSTRAINT [FK_Profile_ToCategories] FOREIGN KEY ([CategoryId]) REFERENCES [Categories]([Id])
+)
